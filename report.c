403c08fe5aac033b7ff0334a755339bc7feef526
ldhrookie <ldhrookie@naver.com>
2026-05-21 15:40:19 +0900

    adf

--- report.c
+++ report.c
@@ -1,8 +1,34 @@
-#ifndef REPORT_H
-#define REPORT_H
+#include <stdio.h>
+#include "report.h"
+#include "personality.h"
 
-#include "file_node.h"
+void writeReport(FileNode* head, const char* filename) {
+    FILE* fp;
+    FileNode* current = head;
 
-void writeReport(FileNode* head, const char* filename);
+    fp = fopen(filename, "w");
 
-#endif
+    if (fp == NULL) {
+        printf("report.txt 파일을 생성할 수 없습니다.\n");
+        return;
+    }
+
+    fprintf(fp, "=====================================\n");
+    fprintf(fp, "           FileSoul Report\n");
+    fprintf(fp, "      파일들의 작은 자기소개서\n");
+    fprintf(fp, "=====================================\n\n");
+
+    while (current != NULL) {
+        fprintf(fp, "[파일명] %s\n", current->name);
+        fprintf(fp, "[확장자] %s\n", current->extension);
+        fprintf(fp, "[크기] %lld bytes\n", current->size);
+        fprintf(fp, "[종류] %s\n", getFileTypeName(current->type));
+        fprintf(fp, "[성격] %s\n", getPersonalityName(current->personality));
+        fprintf(fp, "[대사] %s\n", current->dialogue);
+        fprintf(fp, "\n-------------------------------------\n\n");
+
+        current = current->next;
+    }
+
+    fclose(fp);
+}
