e16f21340780b4a257b4542712f35b4ad6f5454e
Catforce-KR <simon33009@gmail.com>
2026-06-01 15:55:02 +0900

    dd
    ff

--- main.c
+++ main.c
@@ -1,26 +1,36 @@
 #include <stdio.h>
+
 #include "file_node.h"
+
 #include "personality.h"
+
 #include "report.h"
 
+#include "dialogue_view.h"
+ 
 int main(void) {
+
     FileNode* head = NULL;
-
+ 
     appendFileNode(&head, createFileNode("homework.c", "c", 3200));
+
     appendFileNode(&head, createFileNode("old_photo.png", "png", 2480000));
+
     appendFileNode(&head, createFileNode("mysterious.tmp", "tmp", 800));
+
     appendFileNode(&head, createFileNode("final_report.docx", "docx", 145000));
+
     appendFileNode(&head, createFileNode("game.exe", "exe", 7300000));
-
+ 
     assignPersonalities(head);
-
+ 
     printFileList(head);
-
-    writeReport(head, "report.txt");
-
+ 
+    showPopupDialogues(head);
+ 
     freeFileList(head);
-
-    printf("report.txt 파일이 생성되었습니다.\n");
-
+ 
     return 0;
+
 }
+ 
